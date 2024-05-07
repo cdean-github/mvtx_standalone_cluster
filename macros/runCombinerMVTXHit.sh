203c1bd99cfddb2c07d51d52bf41b474da1564ab
cdean-github <cameron.dean@cern.ch>
2024-05-07 17:32:05 -0400

    Updated for cluster reading

---
+++ macros/runCombinerMVTXHit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied."
+    echo "You must at least specify a run number."
+    echo "The second argument is the number of events. If no argument is set, all events will be processed."
+    exit 1
+fi
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+
+#export HOME=/sphenix/u/${LOGNAME}
+export HOME=/sphenix/u/cdean
+export SPHENIX=$HOME/sPHENIX
+export MYINSTALL=$SPHENIX/install
+export LD_LIBRARY_PATH=$MYINSTALL/lib:$LD_LIBRARY_PATH
+export ROOT_INCLUDE_PATH=$MYINSTALL/include:$ROOT_INCLUDE_PATH
+
+source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
+
+nEvents=0
+if [[ "$2" != "" ]]; then
+    nEvents="$2"
+fi
+
+echo running: runCombiner.sh
+root.exe -q -b Fun4All_writeHit.C\($nEvents,\"$1\"\)
+echo Script done
